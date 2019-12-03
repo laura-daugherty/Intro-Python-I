c4546064f9d70c9a78d9fb820128a49a2563087c
Laura Daugherty <lauradaugherty6@gmail.com>
2019-12-03 14:01:32 -0800

    deleting old work

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,16 +35,18 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-waypoints.append({"lat": 44,"lon": -122,"name": "a fourth place"})
+waypoints.append({"lat": 44, "lon": -133, "name": "a fourth place"})
 print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
-waypoints[0] = {"lat": 44,"lon": -122,"name": "not a real place"}
+waypoints[0] = {"lat": 44, "lon": -130, "name": "not a real place"}
 print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
-for x in waypoints
-    print(x)
+
+
+# a = [print((f"{i.lat} {i.lon} {i.name}")) for i in waypoints]
+# print(a)
--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,7 +5,7 @@
 x = 12
 
 def changeX():
-    global x
+    x
     x = 99
 
 changeX()
@@ -20,7 +20,7 @@ def outer():
     y = 120
 
     def inner():
-        nonlocal y
+        y
         y = 999
 
     inner()
